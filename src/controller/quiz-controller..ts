d95eb5204ef7774cbb15ff3433663388dee3bbed
MaryAnzh <maryang@mail.ru>
2023-09-26 20:13:36 +0300

    deploy

--- src/controller/quiz-controller..ts
+++
@@ -1,5 +0,0 @@
-export class QuizController {
-    constructor(data) {
-
-    }
-}
--- src/module.d.ts
+++
@@ -1,5 +0,0 @@
-//подключение css как модуля, доополнителонь прописывается в json
-declare module "*.css";
-declare module "*.ts";
-declare module "*.png";
-declare module "*.svg";
