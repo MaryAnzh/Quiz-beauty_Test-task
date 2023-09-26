1a6ad0f71a3089f59f1d337fba30d18d49a22e54
MaryAnzh <maryang@mail.ru>
2023-09-26 18:13:02 +0300

    feat: add styles for slider boxes (input + label)

--- src/app/pages/quiz/quiz.ts
+++ src/app/pages/quiz/quiz.ts
@@ -1,17 +1,25 @@
+import { IRound, quizData } from '../../../data/quiz.data';
 import { Control } from '../../../utile/control';
 import { quizConst } from './quiz.const';
+import { Round } from './round/round';
 
 export class Quiz extends Control {
     private title: Control;
     private description: Control;
+    public round: Round | null = null;
 
     constructor(parent: HTMLElement) {
         super(parent, 'section', 'quiz');
         this.title = new Control(this.node, 'h2', 'quiz__title', quizConst.title);
         this.description = new Control(this.node, 'p', 'quiz__description', quizConst.description);
+        this.showRound(quizData[0]);
     }
 
-    showRound = (questionNum: number) => { }
+    showRound = (data: IRound) => {
+        if(!this.round) {
+            this.round = new Round(this.node, data);
+        }
+    }
 
     destroy(): void {
         super.destroy();
--- src/app/pages/quiz/round/round.ts
+++ src/app/pages/quiz/round/round.ts
@@ -2,7 +2,26 @@ import { Control } from '../../../../utile/control'
 import { IRound } from '../../../../data/quiz.data';
 
 export class Round extends Control {
+    sliderCheckboxes: Control;
+    checkboxesSet: Control[] = [];
+
     constructor(parent: HTMLElement, data: IRound) {
         super(parent, 'div', 'round');
+        this.sliderCheckboxes = new Control(this.node, 'div', 'round__slider-checkboxes');
+        [...Array(3).keys()].forEach(el => {
+            const box = new Control(this.sliderCheckboxes.node, 'div', 'round__slider-checkboxes__box');
+            const input = new Control(box.node, 'input', 'round__slider-checkboxes__box__input', null,
+                [
+                    { name: 'name', value: 'box' },
+                    { name: 'type', value: 'radio' },
+                    { name: 'id', value: `box-${el}` }
+                ])
+            const label = new Control(box.node, 'label', 'round__slider-checkboxes__box__label', null, [{ name: 'for', value: `box-${el}` }]);
+
+        });
+    }
+
+    destroy(): void {
+        super.destroy();
     }
 }
